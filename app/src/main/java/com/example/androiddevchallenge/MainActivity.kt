50723f51090588a07f34245c6fbe9896a84472ad
zinc <zzinc357@gmail.com>
2021-03-05 16:10:43 +0800

    ui

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,14 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -40,8 +43,31 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text(text = "Countdown timer") })
+            },
+            content = {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CountDownTimer()
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        shape = CircleShape,
+                        onClick = { /*TODO*/ }) {
+                        Text(text = "GO")
+                    }
+                }
+            })
     }
+}
+
+@Composable
+fun CountDownTimer() {
+    Text(text = "倒计时 3 2 1 !")
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
